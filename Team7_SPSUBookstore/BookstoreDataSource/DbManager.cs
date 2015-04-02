6b1581dee236a5988322fd1b0db86f664065b20e
Stephen Strickland <stephen@startup-rescue.com>
2015-04-02 15:38:22 -0400

    Adds entities project for models. Adds DataSource project to manage books and users.

---
+++ Team7_SPSUBookstore/BookstoreDataSource/DbManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookstoreDataSource
+{
+    public class DbManager
+    {
+        public DbManager(string userUrl, string bookUrl)
+        {
+
+        }
+
+
+        public void ReadFiles(string userUrl, string bookUrl)
+        {
+
+        }
+    }
+}
