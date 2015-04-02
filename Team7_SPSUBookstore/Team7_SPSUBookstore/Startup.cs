d15db30b316568d7e9cd4250febae2391ab43401
Stephen Strickland <stephen@startup-rescue.com>
2015-04-02 14:44:00 -0400

    Adds base MVC project.

---
+++ Team7_SPSUBookstore/Team7_SPSUBookstore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Team7_SPSUBookstore.Startup))]
+namespace Team7_SPSUBookstore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
