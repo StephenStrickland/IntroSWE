9fdc345994d443d32425669364130b4810f076d4
wgambrell <tylergam@yahoo.com>
2015-04-13 22:17:29 -0400

    changes to search

--- Team7_SPSUBookstore/Team7_SPSUBookstore/Controllers/SearchController.cs
+++ Team7_SPSUBookstore/Team7_SPSUBookstore/Controllers/SearchController.cs
@@ -17,33 +17,87 @@ namespace Team7_SPSUBookstore.Controllers
            // ViewBag.Title = criteria;
            // return View();
         //}
-        public ActionResult Index(string query)//, int sortOrder)
+        public ActionResult Index(SearchCriteria AdvCriteria)//, int sortOrder)
         {
-            query.ToLower();
-            String[] criteria = query.Split(' ');
+            bool advanced = AdvCriteria.isAdvanced;
+            AdvCriteria.BasicSearch = AdvCriteria.BasicSearch.ToLower();
+            String[] criteria = AdvCriteria.BasicSearch.Split(' ');
             List<String> criteriaList = new List<String>(criteria);
             criteriaList.Remove("the");
             criteriaList.Remove("of");
             criteriaList.Remove("a");
             List<BookDatabaseItem> bookList = DbManager.Books.ToList();
             List<String> searchResultsISBNList = new List<String>();
+           
+                foreach (var c in criteriaList)
+                {
+                    if (c.Any(char.IsDigit) && (c.Length == 13|| c.Length == 14))
+                    {
+                        var firstHalf = c.Substring(0, 3);
+                        var secondHalf = c.Substring(3);
+
+                         var book = bookList.Where(x => (x.ISBN.ToLower().Contains(firstHalf + "-" + secondHalf) || (x.ISBN.ToLower().Contains(c)))).Select(x => x.ISBN).ToList();
+                        if (book.Count == 1)
+                        {
+                            return RedirectToAction("Index", "Book", new {id = book.FirstOrDefault()});
+                        }
+                    
+                    }
+             
+                }
+                if (advanced)
+                  {
+                    if (!String.IsNullOrEmpty(AdvCriteria.Semester))
+                    {
+
+                       bookList = bookList.Where(x => x.Semester.ToLower().Contains(AdvCriteria.Semester)).ToList();
+                        
+                    }
+                    if (!String.IsNullOrEmpty(AdvCriteria.Course))
+                    {
+                        bookList = bookList.Where(x => x.Course.ToLower().Contains(AdvCriteria.Course)).ToList();
+                    }
+                    if (AdvCriteria.Section != 0)
+                    {
+                        bookList = bookList.Where(x => x.Section.Equals(AdvCriteria.Section)).ToList();
+                    }
+                    if (!String.IsNullOrEmpty(AdvCriteria.Professor))
+                    {
+                        bookList = bookList.Where(x => x.Professor.ToLower().Contains(AdvCriteria.Professor)).ToList();
+
+                    }
+                   
+                    if (AdvCriteria.CRN != 0)
+                    {
+                        bookList = bookList.Where(x => x.CRN.Equals(AdvCriteria.CRN)).ToList();
+
+                    }
+                    if (AdvCriteria.isRequired == true)
+                    {
+                        bookList = bookList.Where(x => x.isRequired.Equals(AdvCriteria.isRequired)).ToList();
+
+                    }
             
+                }
+                var tempBookList = new List<BookDatabaseItem>();
 
             foreach(var c in criteriaList)
             {
-                if (c.Any(char.IsDigit) && c.Length == 13)
-                {
-                    var firstHalf = c.Substring(0, 3);
-                    var secondHalf = c.Substring(3);
-                     
-                    searchResultsISBNList.AddRange(bookList.Where(x => x.ISBN.ToLower().Contains( firstHalf + "-" + secondHalf )).Select(x => x.ISBN).ToList());
-                } 
+
+                   tempBookList.AddRange( bookList.Where(x => x.Title.ToLower().Contains(c)).ToList());
                 
-                searchResultsISBNList.AddRange(bookList.Where(x => x.Title.ToLower().Contains(c)).Select(x => x.ISBN).ToList());
-                searchResultsISBNList.AddRange(bookList.Where(x => x.Author.ToLower().Contains(c)).Select(x => x.ISBN).ToList());
-                searchResultsISBNList.AddRange(bookList.Where(x => x.ISBN.ToLower().Contains(c)).Select(x => x.ISBN).ToList()); 
+             
+                   tempBookList.AddRange( bookList.Where(x => x.Author.ToLower().Contains(c)).ToList());
+              
+
+        
             }
-            searchResultsISBNList= searchResultsISBNList.Distinct().ToList();
+            if (tempBookList.Count > 0)
+                bookList = tempBookList;
+            if (bookList.Count == 1)
+            {
+                RedirectToAction("Index", "Book", bookList.FirstOrDefault().ISBN);
+            }
 
             Console.Write("aefaefawfe");
             //switch (sortOrder)
@@ -79,9 +133,9 @@ namespace Team7_SPSUBookstore.Controllers
         }
 
         [HttpPost]
-        public ActionResult AdvSearch(String query, SearchCriteria ADVcriteria)
+        public ActionResult AdvSearch(String query, SearchCriteria AdvCriteria)
         {
-            bool advanced = ADVcriteria.isAdvanced;
+            bool advanced = AdvCriteria.isAdvanced;
             query.ToLower();
             String[] criteria = query.Split(' ');
             List<String> criteriaList = new List<String>(criteria);
@@ -102,34 +156,37 @@ namespace Team7_SPSUBookstore.Controllers
 
                         searchResultsISBNList.AddRange(bookList.Where(x => x.ISBN.ToLower().Contains(firstHalf + "-" + secondHalf)).Select(x => x.ISBN).ToList());
                     }
-                    if (ADVcriteria.Course != null)
+             
+                    if (!String.IsNullOrEmpty(AdvCriteria.Semester))
                     {
-                        searchResultsISBNList.AddRange(bookList.Where(x => x.Course.ToLower().Contains(ADVcriteria.Course)).Select(x => x.ISBN).ToList());
-                    }
-                    if (ADVcriteria.Section != 0)
-                    {
-                        searchResultsISBNList.AddRange(bookList.Where(x => x.Section.Equals(ADVcriteria.Section)).Select(x => x.ISBN).ToList());
-                    }
-                    if (ADVcriteria.Professor != null)
-                    {
-                        searchResultsISBNList.AddRange(bookList.Where(x => x.Professor.ToLower().Contains(ADVcriteria.Professor)).Select(x => x.ISBN).ToList());
+                        searchResultsISBNList.AddRange(bookList.Where(x => x.Semester.ToLower().Contains(AdvCriteria.Semester)).Select(x => x.ISBN).ToList());
 
                     }
-                    if (ADVcriteria.Semester != null)
+                    if (!String.IsNullOrEmpty(AdvCriteria.Course))
                     {
-                        searchResultsISBNList.AddRange(bookList.Where(x => x.Semester.ToLower().Contains(ADVcriteria.Semester)).Select(x => x.ISBN).ToList());
+                        searchResultsISBNList.AddRange(bookList.Where(x => x.Course.ToLower().Contains(AdvCriteria.Course)).Select(x => x.ISBN).ToList());
+                    }
+                    if (AdvCriteria.Section != 0)
+                    {
+                        searchResultsISBNList.AddRange(bookList.Where(x => x.Section.Equals(AdvCriteria.Section)).Select(x => x.ISBN).ToList());
+                    }
+                    if (!String.IsNullOrEmpty(AdvCriteria.Professor))
+                    {
+                        searchResultsISBNList.AddRange(bookList.Where(x => x.Professor.ToLower().Contains(AdvCriteria.Professor)).Select(x => x.ISBN).ToList());
 
                     }
-                    if (ADVcriteria.Course != null)
+                   
+                    if (AdvCriteria.CRN != 0)
                     {
-                        searchResultsISBNList.AddRange(bookList.Where(x => x.Course.ToLower().Contains(ADVcriteria.Course)).Select(x => x.ISBN).ToList());
+                        searchResultsISBNList.AddRange(bookList.Where(x => x.CRN.Equals(AdvCriteria.CRN)).Select(x => x.ISBN).ToList());
 
                     }
-                    if (ADVcriteria.CRN != 0)
+                    if (AdvCriteria.isRequired == true)
                     {
-                        searchResultsISBNList.AddRange(bookList.Where(x => x.CRN.Equals(ADVcriteria.CRN)).Select(x => x.ISBN).ToList());
+                        searchResultsISBNList.AddRange(bookList.Where(x => x.isRequired.Equals(AdvCriteria.isRequired)).Select(x => x.ISBN).ToList());
 
                     }
+            
                 }
                 searchResultsISBNList = searchResultsISBNList.Distinct().ToList();
 
@@ -139,36 +196,12 @@ namespace Team7_SPSUBookstore.Controllers
             return View(bookList.ToList());
         }
 
-        //
-        // GET: /Search/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
 
-        //
-        // POST: /Search/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //public ActionResult ToBookPage()
+        //{
 
-        //
-        // GET: /Search/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
+        //}
 
         //
         // POST: /Search/Edit/5
