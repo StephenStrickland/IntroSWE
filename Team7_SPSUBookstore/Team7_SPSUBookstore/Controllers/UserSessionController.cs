b8d5a6d608c92b801d59c8318195df2a5d0f8116
Stephen Strickland <stephen@startup-rescue.com>
2015-04-14 21:15:43 -0400

    Adds login and guest methods for userController.

--- Team7_SPSUBookstore/Team7_SPSUBookstore/Controllers/UserSessionController.cs
+++ Team7_SPSUBookstore/Team7_SPSUBookstore/Controllers/UserSessionController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Entities;
 
 namespace Team7_SPSUBookstore.Controllers
 {
@@ -10,9 +11,25 @@ namespace Team7_SPSUBookstore.Controllers
     {
         //
         // GET: /UserSession/
-        public ActionResult Index()
+        public ActionResult Login(string email, string password)
+        {
+
+            User user = DbManager.Users.Where(x => x.Email == email).FirstOrDefault();
+            if(user == null ||  user == default(User))
+            {
+                ModelState.AddModelError("", "Invalid username or password.");
+            }
+
+            return View();
+        }
+
+
+        public ActionResult Guest(string email)
         {
             return View();
         }
+
+
+
 	}
 }
