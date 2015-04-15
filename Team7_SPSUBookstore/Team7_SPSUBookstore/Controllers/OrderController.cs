783a1eb0dcdd43fc856f58f3c5327e09b4b8b7dc
William Patrick <admin@muffledsilence.com>
2015-04-14 21:16:18 -0400

    I swear to god I didnt break the build
    Stephen is lying yo.

--- Team7_SPSUBookstore/Team7_SPSUBookstore/Controllers/OrderController.cs
+++ Team7_SPSUBookstore/Team7_SPSUBookstore/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using BookstoreDataSource;
 using Entities;
 using System;
 using System.Collections.Generic;
@@ -52,24 +53,41 @@ namespace Team7_SPSUBookstore.Controllers
         [HttpPost]
         public ActionResult Receipt()
         {
-            Order o = (Order) Session["Order"];
+            Order o = (Order)Session["Order"];
             Session["Order"] = null;
             Session["ShoppingCart"] = null;
+            //GenerateReceipt(o);
             return View(o);
         }
 
-        public string GenerateReceipt(Order o)
+        public void GenerateReceipt(Order o)
         {
             StringBuilder sb = new StringBuilder();
-            sb.Append("-------------------------");
-            sb.Append("Shipping Info");
-            sb.Append("Billing Info"):
-            for(ShoppingCartBook book in o.BooksInCart  )
+            DbManager test = new DbManager("Resources/users.txt", "Resources/books.xlsx");
+
+            sb.AppendLine("-------------------------");
+            sb.AppendLine("Shipping Info");
+            sb.AppendLine("First Name: " + o.ShippingInfo.FirstName);
+            sb.AppendLine("Last Name: " + o.ShippingInfo.LastName);
+            sb.AppendLine("Street Address: " + o.ShippingInfo.StreetAddr);
+            sb.AppendLine("City: " + o.ShippingInfo.City + "State: " + o.ShippingInfo.State + " Zip Code: " + o.ShippingInfo);
+            sb.AppendLine();
+            sb.AppendLine("Billing Info");
+            sb.AppendLine("First Name: " + o.PaymentInfo.FirstName);
+            sb.AppendLine("Last Name: " + o.PaymentInfo.LastName);
+            sb.AppendLine("Street Address: " + o.PaymentInfo.StreetAddr);
+            sb.AppendLine("City: " + o.PaymentInfo.City + "State: " + o.PaymentInfo.State + " Zip Code: " + o.PaymentInfo.ZipCode);
+            sb.AppendLine("-------------------------");
+            foreach (ShoppingCartBook book in o.BooksInCart)
             {
-                
+                string title = test.Books.ToList().Where(x => x.ISBN.Equals(book.ISBN)).First().Title;
+                sb.AppendLine("Title: " + title);
+                sb.AppendLine("ISBN: " + book.ISBN);
+                sb.AppendLine("Type: " + book.TypeInCart);
+                sb.AppendLine("Quantity: " + book.QuantityInCart);
+                sb.AppendLine("Price: " + book.Price);
             }
-            string receipt = "";
-            return "";
+            System.IO.File.WriteAllText("Resources/Receipts/test.txt", sb.ToString());
         }
 
 
