a3893905193b79ce3530229badf9a056a5be3cdf
Stephen Strickland <stephen@startup-rescue.com>
2015-04-23 02:25:39 -0400

    Finishes unit tests for Book, Login

--- Team7_SPSUBookstore/Team7_UnitTest/UnitTest1.cs
+++ Team7_SPSUBookstore/Team7_UnitTest/UnitTest1.cs
@@ -2,22 +2,125 @@
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Web.Mvc;
-using System.Web;
 using System.Web.WebPages;
 using Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Team7_SPSUBookstore.Controllers;
+using System.IO;
+using System.Net;
+using System.Web;
+using System.Web.SessionState;
+using System.Reflection;
+using System.Web.Routing;
 
 namespace Team7_UnitTest
 {
     [TestClass]
     public class UnitTest1
     {
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            // We need to setup the Current HTTP Context as follows:            
+
+            // Step 1: Setup the HTTP Request
+            var httpRequest = new HttpRequest("", "http://localhost/", "");
+
+            // Step 2: Setup the HTTP Response
+            var httpResponce = new HttpResponse(new StringWriter());
+
+            // Step 3: Setup the Http Context
+            var httpContext = new HttpContext(httpRequest, httpResponce);
+            var sessionContainer =
+                new HttpSessionStateContainer("id",
+                                               new SessionStateItemCollection(),
+                                               new HttpStaticObjectsCollection(),
+                                               10,
+                                               true,
+                                               HttpCookieMode.AutoDetect,
+                                               SessionStateMode.InProc,
+                                               false);
+            httpContext.Items["AspSession"] =
+                typeof(HttpSessionState)
+                .GetConstructor(
+                                    BindingFlags.NonPublic | BindingFlags.Instance,
+                                    null,
+                                    CallingConventions.Standard,
+                                    new[] { typeof(HttpSessionStateContainer) },
+                                    null)
+                .Invoke(new object[] { sessionContainer });
+
+            // Step 4: Assign the Context
+            HttpContext.Current = httpContext;
+        }
+
+
+
+
+
         [TestMethod]
         public void TestMethod1()
         {
             Assert.AreEqual("", "");
         }
+
+
+
+        [TestMethod]
+        public void TestBook_Index()
+        {
+            //Glad to see MVC refuses to work
+
+            string expectedBookTitle = "Essentials of Software Engineering 3rd Edition";
+            var controller = new BookController();
+
+            var result = controller.Index("978-1449691998") as ViewResult;
+
+            var book = result.Model as BookDatabaseItem;
+
+            Assert.IsNotNull(result, "Should have returned a ViewResult");
+            Assert.AreEqual(expectedBookTitle, book.Title, "Book Title should have been {0}", expectedBookTitle);
+        }
+
+        [TestMethod]
+        public void TestUserSession_Login()
+        {
+            var controller = new UserSessionController();
+            var wrapper = new HttpContextWrapper(HttpContext.Current);
+            controller.ControllerContext = new ControllerContext(wrapper, new RouteData(), controller);
+
+            var result = controller.Login("email@spsu.edu", "1234568") as ViewResult;
+
+            Assert.AreEqual(true, controller.Session["isLoggedIn"]);
+        }
+
+        [TestMethod]
+        public void TestUserSession_Guest()
+        {
+            //Glad to see MVC refuses to work
+
+            var controller = new UserSessionController();
+            var wrapper = new HttpContextWrapper(HttpContext.Current);
+            controller.ControllerContext = new ControllerContext(wrapper, new RouteData(), controller);
+
+            var result = controller.Guest("email@spsu.edu") as ViewResult;
+            Assert.AreEqual(true, controller.Session["isLoggedIn"]);
+        }
+
+        [TestMethod]
+        public void TestUserSession_Logoff()
+        {
+            //Glad to see MVC refuses to work
+
+            var controller = new UserSessionController();
+            var wrapper = new HttpContextWrapper(HttpContext.Current);
+            controller.ControllerContext = new ControllerContext(wrapper, new RouteData(), controller);
+
+            var result = controller.LogOff("email@spsu.edu") as ViewResult;
+            Assert.AreEqual(false, controller.Session["isLoggedIn"]);
+        }
+
 
         [TestMethod]
         public void TestOrder()
