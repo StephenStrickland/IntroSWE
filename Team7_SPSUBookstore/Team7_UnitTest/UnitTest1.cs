d68683ad8dc83a903d4ad6071158c7fceac89b70
Muffled Silence <admin@muffledsilence.com>
2015-04-22 23:02:43 -0400

    William's Basic tests
    not much

--- Team7_SPSUBookstore/Team7_UnitTest/UnitTest1.cs
+++ Team7_SPSUBookstore/Team7_UnitTest/UnitTest1.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Team7_SPSUBookstore.Controllers;
 
 namespace Team7_UnitTest
 {
@@ -11,5 +15,25 @@ namespace Team7_UnitTest
         {
             Assert.AreEqual("", "");
         }
+
+        [TestMethod]
+        public void TestOrder()
+        {
+            List<ShoppingCartBook> bookList = new List<ShoppingCartBook>();
+            ShoppingCartBook testBook = new ShoppingCartBook();
+            testBook.ISBN = "123";
+            testBook.QuantityInCart = 1;
+            testBook.TypeInCart = StockType.New;
+            testBook.Price = 1;
+
+            bookList.Add(testBook);
+
+            Order testOrder = new Order(bookList);
+
+            Assert.AreEqual(testOrder.SubTotalCost, 1);
+            Assert.AreEqual(testOrder.Tax, (decimal)0.07);
+            Assert.AreEqual(testOrder.TotalCost, (decimal)1.07);
+        }
+
     }
 }
